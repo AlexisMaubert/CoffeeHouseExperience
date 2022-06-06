977f1d5f944598270fa87fa0c5cd4468d3325c30
Alexis J Maubert <alexis.maubert@davinci.edu.ar>
2022-06-06 17:24:43 -0300

    borrar parte no utilizada en el codigo

--- js/scroll.js
+++ js/scroll.js
@@ -4,12 +4,4 @@ $(function () {
       $but.toggleClass('scrolled', $(this).scrollTop() > $but.height());
     });
   });
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+  
