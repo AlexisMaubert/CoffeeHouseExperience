18f4ab79549f07ff2834cf3b057ec97a0cc2389b
Alexis J Maubert <alexis.maubert@davinci.edu.ar>
2022-06-09 11:00:55 -0300

    Cambiar el manejo de los productos al controlador

--- pages/productos.php
+++ pages/productos.php
@@ -1,9 +1,23 @@
 <?php
     require_once('../conf/conf.php');
+    require_once('../modelos/Producto.php');
+    require_once('../modelos/Cnx.php');
     
+    try {
+        $cnx = new Cnx();
+    } catch (PDOException $e) {
+        echo 'Falló la conexión';
+        exit;
+    }
 
     $controlador = 'productos';
 
-    require_once('../vistas/productos.php');
+    $categorias = Producto::$categorias;
+    
+    foreach ($categorias as $categoria) {
+        $todosProductos["$categoria"]= (Producto::mostrarProducto($cnx, $categoria));
+    }
 
+    require_once('../vistas/productos.php');
+    
     ?>
--- vistas/productos.php
+++ vistas/productos.php
@@ -16,25 +16,16 @@
     require_once('../vistas/_bannerAndNav.php');   // Banner con nav integrado
     require_once('../vistas/_carrito.php');              // Carrito de compra
     require_once('../vistas/_volverArriba.php');       // Botón volver a arriba 
-    require_once('../modelos/Cnx.php');
-    require_once('../modelos/Producto.php');
-
-    try {
-        $cnx = new Cnx();
-    } catch (PDOException $e) {
-        echo 'Falló la conexión';
-        exit;
-    }
     ?>
+    
 
     <main class="contenedor padding">
         <h2 class="header-h titulo-productos">Nuestros Productos</h2>
-        <?php foreach (Producto::$categorias as $categoria) : ?>
-            <?php $objCategoria = Producto::mostrarProducto($cnx, $categoria) ?>
+        <?php foreach ($categorias as $categoria) : ?>
             <h3 class="titulo-productos"><?php echo $categoria ?></h3>
             <div class="productos">
-                <?php foreach ($objCategoria as $producto) {
-                    require('../vistas/_producto.php');
+                <?php foreach ($todosProductos["$categoria"] as $producto ) {
+                        require('../vistas/_producto.php');
                 } ?>
             </div><!-- Fin productos -->
         <?php endforeach ?>
